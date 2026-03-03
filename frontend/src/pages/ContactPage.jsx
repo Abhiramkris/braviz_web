1f8d50efef6f905de827dafbc87736149a74031f
Abhiram Krishna <kabhiram67@gmail.com>
2026-03-03 19:38:13 +0530

    change adress

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -75,7 +75,7 @@ const ContactPage = () => {
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
                         </div>
                         <h4 className="font-bold text-slate-800 mb-1 font-inter">Office</h4>
-                        <p className="text-slate-500 font-inter text-sm leading-tight">Building No. 7334, King Fahad Ibn Abdulaziz Saud, Secondary No. 5423, <br />Al Hizam Adh Dhahabi Dist. Postal Code 34432,  <br /> Al khobar, Saudi Arabia</p>
+                        <p className="text-slate-500 font-inter text-sm leading-tight"> Dammam  <br /> Kingdom of Saudi Arabia</p>
                     </div>
                 </motion.div>
             </div>
